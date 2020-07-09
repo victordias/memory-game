df5fb6340d244e13188cc39050ab69d500912468
Victor Oliveira <victor.dias.oliveira@gmail.com>
2020-07-09 00:42:12 -0300

    change test describe text

--- tests/e2e/specs/home.js
+++ tests/e2e/specs/home.js
@@ -4,15 +4,17 @@ describe('Home Page', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
     cy.contains('h1', 'Obliviate!');
+    cy.wait(1000);
   });
 
-  it('Start game by click in button', () => {
+  it('Go to Game from click in button', () => {
     cy.visit('/');
     cy.get('.presentation__startgame').click();
     cy.contains('h1', 'Vamos começar?');
+    cy.wait(1000);
   });
 
-  it('Open Ranking by button', () => {
+  it('Go to Ranking from button click', () => {
     cy.visit('/');
     cy.get('.ranking-link').click();
     cy.contains('h1', 'Hall da Fama');
